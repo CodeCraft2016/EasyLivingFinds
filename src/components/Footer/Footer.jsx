05af60271192591f1016746ff33154546bcca679
CodeCraft2016 <136589932+CodeCraft2016@users.noreply.github.com>
2023-08-17 17:41:09 +0200

    Add GetStarted components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = ()=>{
+    return ( <div>
+        <section className="f-wrapper">
+            <div className="paddings innerWidth flexCenter f-container"></div>
+        </section>
+    </div>)
+}
+
+export default Footer
